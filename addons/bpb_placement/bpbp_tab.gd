9e9898c1bcec7abcc030fee5366ca74f52d4d7e9
babypandabear3 <manusiatidakbiasa@gmail.com>
2021-09-23 21:04:09 +0800

    trial 2

--- addons/bpb_placement/bpbp_tab.gd
+++ addons/bpb_placement/bpbp_tab.gd
@@ -5,15 +5,8 @@ var files = []
 
 onready var item_list : ItemList = $VBoxContainer/ItemList
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_btn_add_button_up():
 	var dialog = EditorFileDialog.new()
@@ -34,14 +27,11 @@ func update_file(path):
 	files.append(path)
 	
 	update_item_list()
-	print("a")
 	
 func update_files(paths):
 	files.clear()
 	for p in paths:
-		print(p)
 		files.append(p)
-	print("b")
 	update_item_list()
 	
 func update_item_list():
--- addons/bpb_placement/plugin.gd
+++ addons/bpb_placement/plugin.gd
@@ -4,47 +4,26 @@ extends EditorPlugin
 const SIDE_PANEL = preload("res://addons/bpb_placement/bpbp_main.tscn")
 var panel = SIDE_PANEL.instance()
 
-var focused_object setget set_focused_object
-var editor_selection
+var button_active : CheckButton
 
 func _enter_tree():
-	editor_selection = get_editor_interface().get_selection()
-	editor_selection.connect("selection_changed", self, "_on_EditorSelection_selection_changed")
-	add_custom_type("BPB_Placement", "Spatial", preload("bpbp_spatial.gd"), preload("icon.png"))
+	button_active = CheckButton.new()
+	button_active.text = "BPB Placement"
+	button_active.connect("button_up", self, "toggle_panel")
+	add_control_to_container(CONTAINER_SPATIAL_EDITOR_MENU, button_active)
 	
 func _exit_tree():
-	remove_custom_type("BPB_Placement")
+	hide_panel()
+	remove_control_from_container(CONTAINER_SPATIAL_EDITOR_MENU, button_active)
 
-func _on_EditorSelection_selection_changed():
-	var selected_nodes = editor_selection.get_selected_nodes()
-	if selected_nodes.size() == 1:
-		var selected_node = selected_nodes[0]
-		if selected_node is BPB_Placement_Spatial:
-			set_focused_object(selected_node)
-			return
-		else:
-			#if editor:
-			#	editor.graphnode_updated()
-			pass
-	set_focused_object(null)
-	
-func set_focused_object(obj):
-	if focused_object != obj:
-		focused_object = obj
-		_on_focused_object_changed(obj)
-
-func _on_focused_object_changed(new_obj):
-	if new_obj:
-		if focused_object is BPB_Placement_Spatial:
-			show_panel()
-		else:
-			hide_panel()
-	else:
-		hide_panel()
-		
 func show_panel():
 	add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_SIDE_LEFT , panel)
 	
 func hide_panel():
 	remove_control_from_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_SIDE_LEFT , panel)
 	
+func toggle_panel():
+	if button_active.pressed:
+		show_panel()
+	else:
+		hide_panel()
